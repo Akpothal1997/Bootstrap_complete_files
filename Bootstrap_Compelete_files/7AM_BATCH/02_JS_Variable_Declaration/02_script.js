585da32e1f9bc4d457af56b28e786bc477ff909d
Ananta kishore pothal <52829830+Akpothal1997@users.noreply.github.com>
2019-10-01 16:33:10 +0530

    added

---
+++ Bootstrap_Compelete_files/7AM_BATCH/02_JS_Variable_Declaration/02_script.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log('The sum of num1 , num2 is : ' + sum); // ES5
+console.log(`The sum of num1 , num2 is : ${sum}`); // ES6
+
+
+let empName = 'John';
+let empAge = 40;
+let empDesignation = 'Manager';
+console.log(`Name : ${empName}
+             Age : ${empAge}
+             Designation : ${empDesignation}`);
+
---
+++ Bootstrap_Compelete_files/7AM_BATCH/05_JS_Conditional_Statements/05_script.js
@@ -0,0 +1,72 @@
+// If Else condition Example
+let time = -5;
+if(time >= 0 &&  time <= 12){
+    console.log('Good Morning');
+}
+else if(time > 12 && time <= 17){
+    console.log('Good Afternoon');
+}
+else if(time > 17 && time <= 23){
+    console.log('Good Evening');
+}
+else{
+    console.log('Please Proper Time Dude')
+}
+
+// Switch Statement Example
+let month = 7;
+let output = 'Month is : ';
+switch(month){
+    case 1:
+        output += 'JANUARY';
+        break;
+    case 2:
+        output += 'FEBRUARY';
+        break;
+    case 3:
+        output += 'MARCH';
+        break;
+    case 4:
+        output += 'APRIL';
+        break;
+    case 5:
+        output += 'MAY';
+        break;
+    case 6:
+        output += 'JUNE';
+        break;
+    case 7:
+        output += 'JULY';
+        break;
+    case 8:
+        output += 'AUGUST';
+        break;
+    case 9:
+        output += 'SEPTEMBER';
+        break;
+    case 10:
+        output += 'OCTOBER';
+        break;
+    case 11:
+        output += 'NOVEMBER';
+        break;
+    case 12:
+        output += 'DECEMBER';
+        break;
+    default:
+        output += 'Enter Proper Month Number Dude';
+        break;
+}
+console.log(output);
+
+
+// For loop Example to display from 1 - 10 values
+
+
+// While loop Example to display from 1 - 10 values
+
+
+// Do while loop Example to display from 1 - 10 values
+
+
+
